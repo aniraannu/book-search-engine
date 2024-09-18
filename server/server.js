5c9243d91a782727fda335f671067971023def0a
aniraannu <51684910+aniraannu@users.noreply.github.com>
2024-09-18 13:03:36 -0700

    Made changes in the server.js to include the apollo server and graphicQL

--- server/server.js
+++ server/server.js
@@ -1,21 +1,40 @@
-const express = require('express');
-const path = require('path');
-const db = require('./config/connection');
-const routes = require('./routes');
+const express = require("express");
+const { ApolloServer } = require("@apollo/server");
+const { expressMiddleware } = require("@apollo/server/express4");
+const path = require("path");
+const db = require("./config/connection");
+//const routes = require('./routes');
+const { typeDefs } = require("./schemas/typeDefs");
+const { resolvers } = require("./schemas/resolvers");
+const { authMiddleware } = require("./utils/auth");
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+const startServer = async () => {
+  await server.start();
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+  app.use("/graphql", expressMiddleware(server, { context: authMiddleware }));
 
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
+  // if we're in production, serve client/build as static assets
+  if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../client/build")));
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "../client/build/index.html"));
+    });
+  }
+  db.once("open", () => {
+    app.listen(PORT, () =>
+      console.log(`🌍 Now listening on localhost:${PORT}`)
+    );
+  });
+};
 
-app.use(routes);
-
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+startServer();
